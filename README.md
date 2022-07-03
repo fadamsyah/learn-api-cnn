dd2c5b4bb0a07b4f74a94546a2aca1ffd345c304
ideapad300 <fadillahadam11@gmail.com>
2022-07-03 14:45:41 +0700

    Create cnn-api

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # learn-api-cnn
+
+## Tools
+- Pytorch
+- Flask
+- PIL
+
+## References
+- [DEPLOYING PYTORCH IN PYTHON VIA A REST API WITH FLASK
+](https://pytorch.org/tutorials/intermediate/flask_rest_api_tutorial.html)
+
+## Sources:
+- [`assets/orange.jpg`](https://indonesian.alibaba.com/product-detail/Fresh-valencia-orange-supply-62469562306.html)
+- [`assets/icecream.jpg`](https://www.thepioneerwoman.com/food-cooking/recipes/a39979824/blueberry-ice-cream-recipe/)
---
+++ test-cnn-api.py
@@ -0,0 +1,20 @@
+import time
+import requests
+
+start = time.time()
+x = requests.post('http://127.0.0.1:5000/predict',
+                  files={'image_bytes': open('assets/orange.jpg', 'rb')})
+end = time.time()
+
+print('Request-response time:', (end-start)*1000, 'ms')
+print('Status code:', x.status_code)
+print('Result:', x.text)
+
+start = time.time()
+x = requests.post('http://127.0.0.1:5000/predict',
+                  files={'image_bytes': open('assets/icecream.jpg', 'rb')})
+end = time.time()
+
+print('Request-response time:', (end-start)*1000, 'ms')
+print('Status code:', x.status_code)
+print('Result:', x.text)
